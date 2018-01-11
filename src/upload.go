e533490766e31277eeb28903a2839f35b7d95315
maxchehab <maxchehab@gmail.com>
2018-01-11 01:03:08 -0800

    Implemented recursive watchdog

---
+++ src/upload.go
@@ -0,0 +1,13 @@
+package main
+
+func remove(file string) {
+
+}
+
+func create(file string) {
+
+}
+
+func change(file string) {
+
+}
---
+++ src/watch.go
@@ -0,0 +1,56 @@
+package main
+
+import (
+	"log"
+
+	"github.com/fsnotify/fsnotify"
+)
+
+func watch(path string) {
+	watcher, err := fsnotify.NewWatcher()
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer watcher.Close()
+
+	done := make(chan bool)
+	go func() {
+		for {
+			select {
+			case event := <-watcher.Events:
+				if event.Op&fsnotify.Remove == fsnotify.Remove || event.Op&fsnotify.Rename == fsnotify.Rename {
+					log.Println("remove file:", event.Name)
+					watcher.Remove(event.Name)
+					remove(event.Name)
+				}
+
+				if event.Op&fsnotify.Create == fsnotify.Create {
+					log.Println("create file:", event.Name)
+					watcher.Add(event.Name)
+					create(event.Name)
+				}
+
+				if event.Op&fsnotify.Write == fsnotify.Write {
+					log.Println("changed file: ", event.Name)
+					change(event.Name)
+				}
+
+			case err := <-watcher.Errors:
+				log.Println("error:", err)
+			}
+		}
+	}()
+	folders := Subfolders(path)
+	for _, folder := range folders {
+		err = watcher.Add(folder)
+		if err != nil {
+			log.Fatal(err)
+		}
+	}
+
+	err = watcher.Add(path)
+	if err != nil {
+		log.Fatal(err)
+	}
+	<-done
+}
